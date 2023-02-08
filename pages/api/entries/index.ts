9b15474709da659acc391aac82ab07ad02c6e3dc
Richard <richardnunez@derco.cl>
2023-02-08 00:11:54 -0300

    Add Entry Post method handler

--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -2,14 +2,15 @@ import { db } from '@/database'
 import { Entry, IEntry } from '@/models'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = { message: string } | IEntry[]
+type Data = { message: string } | IEntry[] | IEntry
 
 
 export default function handler (req: NextApiRequest, res: NextApiResponse<Data>) {
   switch (req.method) {
     case 'GET':
       return getEntries( res )
-
+    case 'POST':
+      return postEntry( req, res )
     default:
       return res.status(400).json({ message: 'Invalid Request by method' })
   }
@@ -18,8 +19,29 @@ export default function handler (req: NextApiRequest, res: NextApiResponse<Data>
 const getEntries = async( res: NextApiResponse<Data> ) => {
   await db.connect()
 
-  const entries = await Entry.find().sort({ createdAt: 'asc' })
+  const entries = await Entry.find().sort({ createdAt: 'ascending' })
 
-  await db.disconnect()
   res.status(200).json( entries )
+  await db.disconnect()
+}
+
+const postEntry = async (req:NextApiRequest, res:NextApiResponse<Data>) => {
+  const { description = '' } = req.body
+  const newEntry = new Entry({
+    description,
+    createdAt: Date.now(),
+  })
+
+  try {
+    await db.connect()
+
+    await newEntry.save()
+
+    await db.disconnect()
+    return res.status(201).json( newEntry )
+  } catch (error) {
+    await db.disconnect()
+    console.log(error)
+    return res.status(500).json({ message: 'Something went wrong'})
+  }
 }
