e0ec2f69816bea5af23814d8157261d0c79ac575
Richard <richardnunez@derco.cl>
2023-02-08 00:14:18 -0300

    Add new entries from frontend to backend

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,7 +1,6 @@
 import { FC, ReactNode, useEffect, useReducer } from 'react'
 import { EntriesContext, entriesReducer } from './'
 import { Entry } from '../../interfaces';
-import { v4 as uuidv4 } from 'uuid';
 import  { entriesApi } from '@/apis';
 
 export interface EntriesState {
@@ -16,17 +15,12 @@ export const EntriesProvider: FC<{ children: ReactNode }> = ({ children }) => {
 
   const [state, dispatch] = useReducer( entriesReducer, Entries_INITIAL_STATE)
 
-  const addNewEntry = ( description: string ) => {
-    const newEntry: Entry = {
-      _id: uuidv4(),
-      description,
-      status: 'pending',
-      createdAt: 1
-    }
+  const addNewEntry = async ( description: string ) => {
+    const { data } = await entriesApi.post<Entry>('/entries', { description })
     
     dispatch({
       type: '[Entry] Add-Entry',
-      payload: newEntry
+      payload: data
     })
   }
 
@@ -38,17 +32,16 @@ export const EntriesProvider: FC<{ children: ReactNode }> = ({ children }) => {
   }
 
   const refreshEntries = async() => {
-    const { data } = await entriesApi.get<Entry>('/entries')
+    const { data } = await entriesApi.get<Entry[]>('/entries')
     dispatch({
       type: '[Entry] Refresh-Data',
-      payload: data
+      payload: data,
     })
   }
 
   useEffect(() => {
     refreshEntries()
   }, [])
-  
 
   return (
     <EntriesContext.Provider 
